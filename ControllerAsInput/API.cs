da59f1105dbe88c1369c299a25612b21aeed400a
zalnaRs <thezalnar@hotmail.com>
2021-02-14 16:46:49 +0100

    Add project files.

---
+++ ControllerAsInput/API.cs
@@ -0,0 +1,188 @@
+﻿using System.Diagnostics;
+using WindowsInput;
+using SharpDX.XInput;
+using System.Threading;
+using System;
+using System.Xml;
+
+namespace ControllerAsInput
+{
+    public class ControllerAsInput
+    {
+        public static int MovementDivider = 1000;
+        public static int ScrollDivider = 20000;
+        public const int RefreshRate = 60;
+
+        public Timer _timer;
+        public Controller _controller;
+        public IMouseSimulator _mouseSimulator;
+        public IKeyboardSimulator _keyboardSimulator;
+
+        public bool _wasADown;
+        public bool _wasBDown;
+        public bool _wasXDown;
+        public bool _wasYDown;
+        public bool _wasBackDown;
+        public bool _wasStartDown;
+        public bool _wasLeftDown;
+        public bool _wasRightDown;
+        public bool _wasUpDown;
+        public bool _wasDownDown;
+        public bool _wasLeftThumbDown;
+        public bool _wasRightShoulderDown;
+        public ControllerAsInput()
+        {
+            _controller = new Controller(UserIndex.One);
+            _mouseSimulator = new InputSimulator().Mouse;
+            _keyboardSimulator = new InputSimulator().Keyboard;
+            _timer = new Timer(obj => Update());
+        }
+
+        private void Update()
+        {
+            _controller.GetState(out var state);
+            Movement(state);
+            Scroll(state);
+            LeftButton(state);
+            RightButton(state);
+            KeyBoardButton(state);
+            KeyBoardCloseButton(state);
+            BackSpaceButton(state);
+            EnterButton(state);
+            ArrowButtons(state);
+            CapsLockButton(state);
+            SpaceButton(state);
+        }
+        private void Movement(State state)
+        {
+            var x = state.Gamepad.LeftThumbX / MovementDivider;
+            var y = state.Gamepad.LeftThumbY / MovementDivider;
+            try
+            {
+                _mouseSimulator.MoveMouseBy(x, -y);
+            } catch { Console.Clear(); }
+            
+        }
+        private void Scroll(State state)
+        {
+            var x = state.Gamepad.RightThumbX / ScrollDivider;
+            var y = state.Gamepad.RightThumbY / ScrollDivider;
+            _mouseSimulator.HorizontalScroll(x);
+            _mouseSimulator.VerticalScroll(y);
+        }
+        private void LeftButton(State state)
+        {
+            var isADown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
+            if (isADown && !_wasADown) _mouseSimulator.LeftButtonDown();
+            if (!isADown && _wasADown) _mouseSimulator.LeftButtonUp();
+            _wasADown = isADown;
+        }
+
+        private void RightButton(State state)
+        {
+            var isBDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B);
+            if (isBDown && !_wasBDown) _mouseSimulator.RightButtonDown();
+            if (!isBDown && _wasBDown) _mouseSimulator.RightButtonUp();
+            _wasBDown = isBDown;
+        }
+        private void KeyBoardButton(State state)
+        {
+
+            var isXDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.X);
+            if (isXDown && !_wasXDown)
+            {
+                try
+                {
+                    Process.Start(@"osk.exe");
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to open on screen keyboard!");
+                }
+            }
+
+            _wasXDown = isXDown;
+        }
+        private void KeyBoardCloseButton(State state)
+        {
+
+            var isYDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y);
+            if (isYDown && !_wasYDown)
+            {
+                try
+                {
+                    Process[] runingProcess = Process.GetProcesses();
+                    int i;
+                    for (i = 0; i < runingProcess.Length; i++)
+                    {
+                        if (runingProcess[i].ProcessName == "osk")
+                        {
+                            runingProcess[i].Kill();
+                        }
+
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Failed to close on screen keyboard!");
+                }
+            }
+
+            isYDown = _wasYDown;
+        }
+        private void BackSpaceButton(State state)
+        {
+
+            var isBackDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Back);
+            if (isBackDown && !_wasBackDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.BACK);
+            if (!isBackDown && _wasBackDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.BACK);
+            _wasBackDown = isBackDown;
+        }
+        private void EnterButton(State state)
+        {
+            
+            var isStartDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Start);
+            if (isStartDown && !_wasStartDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.RETURN);
+            if (!_wasStartDown && isStartDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.RETURN);
+            _wasStartDown = isStartDown;
+        }
+        private void ArrowButtons(State state)
+        {
+
+            var isLeftDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft);
+            if (isLeftDown && !_wasLeftDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.LEFT);
+            if (!_wasLeftDown && isLeftDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.LEFT);
+            _wasLeftDown = isLeftDown;
+            var isRightDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight);
+            if (isRightDown && !_wasRightDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.RIGHT);
+            if (!_wasRightDown && isRightDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.RIGHT);
+            _wasRightDown = isRightDown;
+            var isUpDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp);
+            if (isUpDown && !_wasUpDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.UP);
+            if (!_wasUpDown && isUpDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.UP);
+            _wasUpDown = isUpDown;
+            var isDownDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown);
+            if (isDownDown && !_wasDownDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.DOWN);
+            if (!_wasDownDown && isDownDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.DOWN);
+            _wasDownDown = isDownDown;
+        }
+        private void CapsLockButton(State state)
+        {
+            var isLeftThumbDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.LeftThumb);
+            if (isLeftThumbDown && !_wasLeftThumbDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.CAPITAL);
+            if (!_wasLeftThumbDown && isLeftThumbDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.CAPITAL);
+            _wasLeftThumbDown = isLeftThumbDown;
+        }
+        private void SpaceButton(State state)
+        {
+            var isRightShoulderDown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.RightShoulder);
+            if (isRightShoulderDown && !_wasRightShoulderDown) _keyboardSimulator.KeyDown(WindowsInput.Native.VirtualKeyCode.SPACE);
+            if (!_wasRightShoulderDown && isRightShoulderDown) _keyboardSimulator.KeyUp(WindowsInput.Native.VirtualKeyCode.SPACE);
+            _wasRightShoulderDown = isRightShoulderDown;
+        }
+        public void Start()
+        {
+            _timer.Change(0, 1000 / RefreshRate);
+        }
+    }
+}
---
+++ ControllerAsInput/Program.cs
@@ -0,0 +1,123 @@
+﻿using SharpDX.XInput;
+using System;
+using System.Threading;
+namespace ControllerAsInput
+{
+    class Program
+    {
+        static bool showMenu = false;
+        static void Main(string[] args)
+        {
+            try {
+                var inputMonitor = new ControllerAsInput();
+                inputMonitor.Start();
+            } catch {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.ReadLine();
+            }
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("ControllerAsInput has started!");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+
+       /* private static System.Timers.Timer BatteryLevel()
+        {
+            System.Timers.Timer tmr = new System.Timers.Timer();
+            tmr.Elapsed += (sender, args) => BatteryLevel();
+            tmr.AutoReset = true;
+            tmr.Interval = 999;
+            tmr.Start();
+            if (SharpDX.XInput.BatteryLevel == SharpDX.XInput.BatteryLevel.Full)
+            {
+
+            }
+            Console.WriteLine("");
+            Console.WriteLine();
+
+            return tmr;
+        }*/
+
+        private static bool MainMenu()
+        {
+           
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Change Cursor Speed");
+            Console.WriteLine("2) Change Scroll Speed");
+            Console.WriteLine("3) Show License");
+            Console.WriteLine("4) Exit");
+            Console.Write("\r\nSelect an option: ");
+            //  BatteryLevel();
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    ChangeCursorSpeed();
+                    return true;
+                case "2":
+                    ChangeScrollSpeed();
+                    return true;
+                case "3":
+                    ShowLicense();
+                    return true;
+                case "4":
+                    Environment.Exit(0);
+                    return false;
+                default:
+                    return true;
+            }
+
+        }
+
+        private static void ChangeCursorSpeed()
+        {
+            Console.WriteLine("Mouse speed: (Default 1000 | Lower is faster)");
+            try
+            {
+                int MouseSpeed = int.Parse(Console.ReadLine());
+                    ControllerAsInput.MovementDivider = MouseSpeed;
+            } catch
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Invalid value!");
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Thread.Sleep(3000);
+            }
+            
+        }
+        private static void ChangeScrollSpeed()
+        {
+            Console.WriteLine("Scroll speed: (Default 20000 | Lower is faster)");
+            try
+            {
+                int ScrollSpeed = int.Parse(Console.ReadLine());
+                ControllerAsInput.ScrollDivider = ScrollSpeed;
+            }
+            catch
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Invalid value!");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+                Thread.Sleep(3000);
+            }
+
+        }
+        private static void ShowLicense()
+        {
+            Console.WriteLine("Scroll speed: (Default 20000 | Lower is faster)");
+            Console.ReadLine();
+        }
+    }
+}
