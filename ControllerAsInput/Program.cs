9df708167116269578f10d8e24753e5318149330
zalnaRs <thezalnar@hotmail.com>
2021-02-15 15:04:07 +0100

    1.1

--- ControllerAsInput/Program.cs
+++ ControllerAsInput/Program.cs
@@ -3,8 +3,12 @@ using System;
 using System.Threading;
 namespace ControllerAsInput
 {
+
+
     class Program
     {
+        static string LocalVersion = "1.1";
+        static string Version = "Unkonow";
         static bool showMenu = false;
         static void Main(string[] args)
         {
@@ -22,10 +26,13 @@ namespace ControllerAsInput
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.White;
             showMenu = true;
+            System.Net.WebClient wc = new System.Net.WebClient();
+            Version = wc.DownloadString("https://pastebin.com/raw/Ygbw2tdu");
             while (showMenu)
             {
                 showMenu = MainMenu();
             }
+
         }
 
        /* private static System.Timers.Timer BatteryLevel()
@@ -47,7 +54,7 @@ namespace ControllerAsInput
 
         private static bool MainMenu()
         {
-           
+            var IfNewVersion = false;
             Thread.Sleep(1000);
             Console.Clear();
             Console.WriteLine("Choose an option:");
@@ -55,25 +62,79 @@ namespace ControllerAsInput
             Console.WriteLine("2) Change Scroll Speed");
             Console.WriteLine("3) Show License");
             Console.WriteLine("4) Exit");
+            if (!Version.Equals(LocalVersion))
+            {
+                IfNewVersion = true;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("5) Update to the new version!");
+                Console.WriteLine("Version: " + LocalVersion + "New version: "+ Version);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            } else
+            {
+                Console.WriteLine("Version: " + LocalVersion);
+            }
+
+            
             Console.Write("\r\nSelect an option: ");
             //  BatteryLevel();
-            switch (Console.ReadLine())
+            if (IfNewVersion)
             {
-                case "1":
-                    ChangeCursorSpeed();
-                    return true;
-                case "2":
-                    ChangeScrollSpeed();
-                    return true;
-                case "3":
-                    ShowLicense();
-                    return true;
-                case "4":
-                    Environment.Exit(0);
-                    return false;
-                default:
-                    return true;
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        ChangeCursorSpeed();
+                        return true;
+                    case "2":
+                        ChangeScrollSpeed();
+                        return true;
+                    case "3":
+                        ShowLicense();
+                        return true;
+                    case "4":
+                        System.Diagnostics.Process[] runingProcess = System.Diagnostics.Process.GetProcesses();
+                        int i;
+                        for (i = 0; i < runingProcess.Length; i++)
+                        {
+                            if (runingProcess[i].ProcessName == "osk")
+                            {
+                                runingProcess[i].Kill();
+                            }
+
+                        }
+                        Environment.Exit(0);
+                        return false;
+                    case "5":
+                        System.Diagnostics.Process.Start("https://github.com/zalnaRs/ControllerAsInput/releases");
+                        Thread.Sleep(25000);
+                        Environment.Exit(0);
+                        return false;
+                    default:
+                        return true;
+                }
+            } else
+            {
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        ChangeCursorSpeed();
+                        return true;
+                    case "2":
+                        ChangeScrollSpeed();
+                        return true;
+                    case "3":
+                        ShowLicense();
+                        return true;
+                    case "4":
+                        Environment.Exit(0);
+                        return false;
+
+                    default:
+                        return true;
+                }
             }
+            
 
         }
 
@@ -89,11 +150,12 @@ namespace ControllerAsInput
                 Console.BackgroundColor = ConsoleColor.Red;
                 Console.ForegroundColor = ConsoleColor.Black;
                 Console.WriteLine("Invalid value!");
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
-                Thread.Sleep(3000);
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
             }
-            
+            Console.WriteLine("\n" + "Press any key to go back...");
+            Console.ReadLine();
+
         }
         private static void ChangeScrollSpeed()
         {
@@ -110,14 +172,29 @@ namespace ControllerAsInput
                 Console.WriteLine("Invalid value!");
                 Console.BackgroundColor = ConsoleColor.Black;
                 Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(3000);
             }
-
-        }
-        private static void ShowLicense()
-        {
-            Console.WriteLine("Scroll speed: (Default 20000 | Lower is faster)");
+            Console.WriteLine("\n" + "Press any key to go back...");
             Console.ReadLine();
         }
+        private static void ShowLicense(){
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(@"LICENSE");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine("\t" + line);
+                }
+            } catch
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Someting went wrong!");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        
+            Console.WriteLine("\n" + "Press any key to go back...");
+            Console.ReadLine();
+         }
     }
 }
--- ControllerAsInput/Properties/AssemblyInfo.cs
+++ ControllerAsInput/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ControllerAsInput")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ControllerAsInput is allowing to use a Xbox (360, one, series s, series x ) controller as mouse!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ControllerAsInput")]
@@ -32,5 +33,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
